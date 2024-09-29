acf87dabe3387fe4b704eb46e3d1bfcb3392b0e4
Asit Patel <patela8@myumanitoba.ca>
2024-09-28 19:45:18 -0500

    add preliminary splash screen

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// Main dark colour. Use this for text, backgrounds
+const Color notBlack = Color(0xff373737); // Hex: #373737
+
+// Theme accent color
+const Color notYellow = Color(0xffeec23e); // Hex: #EEC23E
+
+// Main light colour. Use this for text, backgrounds
+const Color notWhite = Color(0xfffefaee); // Hex: #FEFAEE
+
+//Use this for contrasting on Not White (text fields)
+const Color almostWhite = Color(0xfffffefa); // Hex: #FFFEFA
