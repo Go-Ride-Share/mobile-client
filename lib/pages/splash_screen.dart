b132eacd0485f3672216230732561556d6ae8676
Asit Patel <patela8@myumanitoba.ca>
2024-10-07 23:17:16 -0500

    misc fix 2

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../theme.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacementNamed('/home'); // Replace '/home' with your target route
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: notYellow, // Replace with your desired color
+      body: Center(
+        child: Image.asset('assets/images/LogoNotBlack.png'), // Ensure you have a logo image at this path
+      ),
+    );
+  }
+}
