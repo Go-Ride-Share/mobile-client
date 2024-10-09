564b0bd185d521478c07a721a6beed3cc99bf005
Asit Patel <patela8@myumanitoba.ca>
2024-10-09 13:02:24 -0500

    remove hardcoded links and use env vars

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -1,19 +1,35 @@
 import 'package:go_ride_sharing/models/post.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'caching_service.dart';
+import '../constants.dart';
 
 class PostService {
-  String logicLayerAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1jN2wzSXo5M2c3dXdnTmVFbW13X1dZR1BrbyIsImtpZCI6Ik1jN2wzSXo5M2c3dXdnTmVFbW13X1dZR1BrbyJ9.eyJhdWQiOiJhcGk6Ly9iNzZmZDJhYi0xN2M5LTQ4MDYtOWY4Mi04ZDExMDRiNjY5OTEiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC80MzM3MGNhZi0xNGY4LTQ4NDktOTE5NC05NTk1MDQ5YzA2ZTkvIiwiaWF0IjoxNzI4NDMyNzQ3LCJuYmYiOjE3Mjg0MzI3NDcsImV4cCI6MTcyODQzNjY0NywiYWlvIjoiazJCZ1lIaFh2VExTY281ZW1oSHZWZmFHVHdjMkFRQT0iLCJhcHBpZCI6ImI3NmZkMmFiLTE3YzktNDgwNi05ZjgyLThkMTEwNGI2Njk5MSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzQzMzcwY2FmLTE0ZjgtNDg0OS05MTk0LTk1OTUwNDljMDZlOS8iLCJvaWQiOiIxMDA0ZjEzNC02ZWE1LTRkMDUtYWM3Zi1jNTNiMThjODcyY2QiLCJyaCI6IjAuQVc4QnJ3dzNRX2dVU1VpUmxKV1ZCSndHNmF2U2I3ZkpGd1pJbjRLTkVRUzJhWkZ2QVFBLiIsInN1YiI6IjEwMDRmMTM0LTZlYTUtNGQwNS1hYzdmLWM1M2IxOGM4NzJjZCIsInRpZCI6IjQzMzcwY2FmLTE0ZjgtNDg0OS05MTk0LTk1OTUwNDljMDZlOSIsInV0aSI6IlZNT096Mzd2OEVDZGt4MTdwRXdDQXciLCJ2ZXIiOiIxLjAifQ.k8Q9A1qUsb8ax1RSjxfqQo24LsqFzu98zv1M-uy8RTbj869htXPnYgQZO1cZmHH0G50LGvcHl239Bsm6jbbI8mfiNdeySh5H6qO8bqShJ0LzmJq96D0flhNaW-O65DtH0IO6FCQ8RSQy_snwWTaEH50aqOTipWtdLTSCiJ5LFVvTgYCRA7B95e9-DP3hp2rQWHXBdRx_rzPDGLojJixh1q4W792WpZWdNEsDuo4GUNY3sh1y5fPGMCrmhBYRy9V15rlJE8U_xkuzjFcmcAAPp9eLQRO3zd3e1pG2uL6wfXeXCCsT6hDUqMT0Ixyz0TM7JMX3GWS810hzIyBXhK444Q";
-  String dbLayerAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1jN2wzSXo5M2c3dXdnTmVFbW13X1dZR1BrbyIsImtpZCI6Ik1jN2wzSXo5M2c3dXdnTmVFbW13X1dZR1BrbyJ9.eyJhdWQiOiJhcGk6Ly9kZDZjY2RmMy0wOTFmLTQ2ZjMtYTRkMi1lOGE1MGI2ZTUyNjciLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC80MzM3MGNhZi0xNGY4LTQ4NDktOTE5NC05NTk1MDQ5YzA2ZTkvIiwiaWF0IjoxNzI4NDMyNzQ3LCJuYmYiOjE3Mjg0MzI3NDcsImV4cCI6MTcyODQzNjY0NywiYWlvIjoiazJCZ1lJaXhXYnpWWUk4RXAxOHVud2FYOGdNakFBPT0iLCJhcHBpZCI6ImRkNmNjZGYzLTA5MWYtNDZmMy1hNGQyLWU4YTUwYjZlNTI2NyIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzQzMzcwY2FmLTE0ZjgtNDg0OS05MTk0LTk1OTUwNDljMDZlOS8iLCJvaWQiOiI5ZjE3YjU3MS0zNGUwLTQxOGItODBjMi00NDU0M2ExZTI4YTkiLCJyaCI6IjAuQVc4QnJ3dzNRX2dVU1VpUmxKV1ZCSndHNmZQTmJOMGZDZk5HcE5Mb3BRdHVVbWR2QVFBLiIsInN1YiI6IjlmMTdiNTcxLTM0ZTAtNDE4Yi04MGMyLTQ0NTQzYTFlMjhhOSIsInRpZCI6IjQzMzcwY2FmLTE0ZjgtNDg0OS05MTk0LTk1OTUwNDljMDZlOSIsInV0aSI6ImZ6Mlhlb0t3bGtpZFVlWHVoNlVLQVEiLCJ2ZXIiOiIxLjAifQ.FwKyqdfSSGlt558O7lcjp7AYEAKiwvKLTO-d9yyqzIrxkXAFBuWcfZQFX1Kkr9_TO7kVZQpmO06qcVhJYmjAMjaAqBEmcmSls97IQoTacPp01TJSPM3GVOYlQHQ9Uyz0EPFjJX0RPsjhCsL6TqeWknxWB0h3QzmQX2zRgUhwXkEg1HfrjUMY7UStRyY54gOFnP8V2TNefsAkKKfSfwETLbSuu-8Rex0cAQ6UfQBc9An7wLxIfK_yHJdpwKxurxc975_Ukeq7x23KpmGKnfpLzkx4IqiwdrUmEv6BWLrmCAlbFgjRxySu2vKOZtRnUQHWD_XhG22sNAEw-tRUOK66ww";
+  CachingService cache = CachingService();
+
+  late Future<String?> logicLayerAccessToken;
+  late Future<String?> dbLayerAccessToken;
+  late Future<String?> userID;
+
+  PostService() {
+    logicLayerAccessToken = cache.getData(ENV.CACHE_BEARER_TOKEN_KEY);
+    dbLayerAccessToken = cache.getData(ENV.CACHE_DB_TOKEN_KEY);
+    userID = cache.getData(ENV.CACHE_USER_ID_KEY);
+  }
 
   Future<List<Post>> fetchProfilePosts() async {
-    final url = Uri.parse('https://grs-logic-dev.azurewebsites.net/api/GetPosts?userId=054c8144-4935-44bf-8887-6a06a9d8a78f');
+    final userId = await userID;
+    final logicToken = await logicLayerAccessToken;
+    final dbToken = await dbLayerAccessToken;
 
+    final url = Uri.parse('${ENV.API_BASE_URL}/api/GetPosts?userId=$userId');
+
+    // logic token and db token can be null values if they are expired. We are ignoring the case of having to sign in again.
     final headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + logicLayerAccessToken,
-      'X-Db-Token': dbLayerAccessToken,
-      'X-User-ID': '715cf2a6-23bb-4068-8a8d-86c2ccd9d044',
+      'Authorization': 'Bearer ${logicToken ?? ''}',
+      'X-Db-Token': dbToken ?? '',
+      'X-User-ID': userId ?? '',
     };
 
     try {
@@ -53,13 +69,13 @@ class PostService {
       'price': post.price,
     });
 
-    final url = Uri.parse('https://grs-logic-dev.azurewebsites.net/api/SavePost?userId=054c8144-4935-44bf-8887-6a06a9d8a78f');
+    final url = Uri.parse('${ENV.API_BASE_URL}/api/SavePost?userId=${await userID}');
 
     final headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + logicLayerAccessToken,
-      'X-Db-Token': dbLayerAccessToken,
-      'X-User-ID': '715cf2a6-23bb-4068-8a8d-86c2ccd9d044',
+      'Authorization': 'Bearer ${await logicLayerAccessToken ?? ''}',
+      'X-Db-Token': await dbLayerAccessToken ?? '',
+      'X-User-ID': await userID ?? '',
     };
 
     print('Post Data: $postData');
@@ -92,13 +108,13 @@ class PostService {
       'price': post.price,
     });
 
-    final url = Uri.parse('https://grs-logic-dev.azurewebsites.net/api/SavePost?userId=054c8144-4935-44bf-8887-6a06a9d8a78f');
+    final url = Uri.parse('${ENV.API_BASE_URL}/api/SavePost?userId=${await userID}');
 
     final headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + logicLayerAccessToken,
-      'X-Db-Token': dbLayerAccessToken,
-      'X-User-ID': '715cf2a6-23bb-4068-8a8d-86c2ccd9d044',
+      'Authorization': 'Bearer ${await logicLayerAccessToken ?? ''}',
+      'X-Db-Token': await dbLayerAccessToken ?? '',
+      'X-User-ID': await userID ?? '',
     };
 
     print('Post Data: $postData');
