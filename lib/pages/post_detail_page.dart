9bd7c21beed7fbb4c25d6b3fea97bdf441dad69e
Asit Patel <patela8@myumanitoba.ca>
2024-12-05 23:42:21 -0600

    finalize styling post details page

--- lib/pages/post_detail_page.dart
+++ lib/pages/post_detail_page.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:go_ride_sharing/widgets/trip_details.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:go_ride_sharing/models/conversation.dart';
 import 'package:go_ride_sharing/models/post.dart';
 import 'package:go_ride_sharing/services/message_service.dart';
-import 'package:go_ride_sharing/theme.dart';
-import 'package:go_ride_sharing/widgets/map_window.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:go_ride_sharing/services/post_service.dart';
 import 'package:go_ride_sharing/pages/conversation_detail_page.dart';
+import 'package:go_ride_sharing/widgets/map_window.dart';
+import 'package:go_ride_sharing/theme.dart';
+
 class PostDetailPage extends StatelessWidget {
   final Post post;
   late final Map<MarkerId, Marker> markers;
 
-
-  //require some conversation details too. If not provided then dont show it.
-  //works perfectly becuase when opened from my post page, contact button doesnt appear
-  //but when clicked from search page, it does appear.
   PostDetailPage({super.key, required this.post}) {
     markers = {
       const MarkerId('Origin'): Marker(
@@ -41,92 +39,105 @@ class PostDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Post Details'),
+        title: const Text('Post Details'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            Text(
-              post.postName,
-              style: const TextStyle(
+            buildInfoCard(
+              title: post.postName,
+              subtitle: post.description,
+              titleStyle: const TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 20.0,
+                fontSize: 22,
+              ),
+              subtitleStyle: const TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
               ),
             ),
-            const SizedBox(height: 8),
-            Text(
-              post.description,
-              style: const TextStyle(fontSize: 16.0),
+            const SizedBox(height: 10),
+            buildInfoCard(
+              title: 'Travel Details',
+              content: TripDetails(post: post),
             ),
-            const SizedBox(height: 16),
-            Text(
-              'Origin: ${post.originName}',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Destination: ${post.destinationName}',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Seats Available: ${post.seatsAvailable}',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Departure Date: ${post.departureDate.toLocal().toString().split(' ')[0]}',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Price: \$${post.price}',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-            const SizedBox(height: 8),
-            if (post.posterName != null)
-              Text(
-              'Posted by: ${post.posterName}',
-              style: const TextStyle(fontSize: 16.0),
-              ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 10),
             MapWindow(markers: markers),
-            const SizedBox(height: 10.0),
-        FutureBuilder<String?>(
-          future: PostService().userID,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasData && snapshot.data != post.posterId) {
-              return ElevatedButton(
-                onPressed: () async {
-                  Conversation conversation =
-                      await MessageService().createConversation(post.posterId);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          ConversationDetailPage(conversation: conversation),
+            const SizedBox(height: 10),
+            FutureBuilder<String?>(
+              future: PostService().userID,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasData && snapshot.data != post.posterId) {
+                  return ElevatedButton(
+                    onPressed: () async {
+                      Conversation conversation = await MessageService()
+                          .createConversation(post.posterId);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ConversationDetailPage(
+                              conversation: conversation),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      backgroundColor: notYellow,
+                      foregroundColor: notBlack,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
+                    child: const Text('Contact'),
                   );
-                },
-                style: FilledButton.styleFrom(
-                    backgroundColor: notYellow,
-                    foregroundColor: notBlack,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    shadowColor: notBlack),
-                child: const Text('Contact'),
-              );
-            } else {
-              return const SizedBox.shrink();
-            }
-          },
-        ),
+                } else {
+                  return const SizedBox.shrink();
+                }
+              },
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildInfoCard({
+    required String title,
+    String? subtitle,
+    TextStyle? titleStyle,
+    TextStyle? subtitleStyle,
+    Widget? content,
+  }) {
+    return Card(
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+      side: const BorderSide(color: notYellow, width: 2),
+      ),
+      shadowColor: notBlack,
+      elevation: 2, // Increased elevation for a more prominent shadow
+      child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text(title,
+          style: titleStyle ??
+            const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: notBlack)),
+        if (subtitle != null) ...[
+          const SizedBox(height: 8),
+          Text(subtitle,
+            style: subtitleStyle ??
+              const TextStyle(fontSize: 16, color: notBlack)),
+        ],
+        if (content != null) ...[
+          const SizedBox(height: 10),
+          content,
+        ],
+        ],
+      ),
       ),
     );
   }
